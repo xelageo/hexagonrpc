xelageo/hexagonrpc

commit f7878ae2c3ed7649e1ceebdc8b1b0aa06545719b

fastrpc: remotectl: add close definition
A remotectl_open() procedure call should be mirrored with a
remotectl_close() procedure call. Add a definition for this remote
procedure so handles can be closed.

fastrpc/fastrpc_remotectl.h
@@ -36,6 +36,7 @@
 	}
 
 DEFINE_REMOTE_PROCEDURE(0, remotectl_open, 0, 1, 2, 1);
+DEFINE_REMOTE_PROCEDURE(0, remotectl_close, 1, 0, 1, 1);
 
 #undef DEFINE_REMOTE_PROCEDURE
 

commit 5671e9d6d092ea3522f1a491a3af4375b6a906ee

fastrpc: remotectl: set correct close() message id
The remotectl_close method does not use the same message ID as the
remotectl_open method. Change the message ID so the correct method is
called on the remote processor.

Fixes: f7878ae2c3ed ("fastrpc: remotectl: add close definition")

fastrpc/fastrpc_remotectl.h
@@ -36,7 +36,7 @@
 	}
 
 DEFINE_REMOTE_PROCEDURE(0, remotectl_open, 0, 1, 2, 1);
-DEFINE_REMOTE_PROCEDURE(0, remotectl_close, 1, 0, 1, 1);
+DEFINE_REMOTE_PROCEDURE(1, remotectl_close, 1, 0, 1, 1);
 
 #undef DEFINE_REMOTE_PROCEDURE
 