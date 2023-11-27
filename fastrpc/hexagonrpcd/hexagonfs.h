e0d6c2a7accc259422a18d9ef3d65de4f6ffd346
Richard Acayan <mailingradian@gmail.com>
2023-11-27 18:46:30 -0500

    fastrpc: hexagonrpcd: hexagonfs: remove static root filesystem
    The root filesystem is now dynamically constructed in rpcd_builder.c.
    Remove this macro-generated filesystem as it is unused and generates a
    warning.

--- fastrpc/hexagonrpcd/hexagonfs.h
+++ fastrpc/hexagonrpcd/hexagonfs.h
@@ -69,8 +69,6 @@ extern struct hexagonfs_file_ops hexagonfs_mapped_sysfs_ops;
 extern struct hexagonfs_file_ops hexagonfs_plat_subtype_name_ops;
 extern struct hexagonfs_file_ops hexagonfs_virt_dir_ops;
 
-extern struct hexagonfs_dirent hexagonfs_root_dir;
-
 int hexagonfs_open_root(struct hexagonfs_fd **fds, struct hexagonfs_dirent *root);
 int hexagonfs_openat(struct hexagonfs_fd **fds, int rootfd, int dirfd, const char *name);
 int hexagonfs_close(struct hexagonfs_fd **fds, int fileno);
