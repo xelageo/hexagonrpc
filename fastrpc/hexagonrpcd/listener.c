xelageo/hexagonrpc

commit 9ea9bed77b18fa17ea59f41c6bfdcbfdb0926749

fastrpc: hexagonrpcd: listener: do not attempt to allocate zero outbufs
The malloc(3p) manual page states that an implementation may choose to
return a non-NULL pointer when the size is zero. This function assumes
that there is an element in the array if the pointer is not NULL. When
the size is zero, skip the allocation and return NULL.

fastrpc/hexagonrpcd/listener.c
@@ -73,6 +73,14 @@ static struct fastrpc_io_buffer *allocate_outbufs(const struct fastrpc_function_
 	off_t off;
 	uint32_t *sizes;
 
+	/*
+	 * POSIX allows malloc to return a non-NULL pointer to a zero-size area
+	 * in memory. Since the code below assumes non-zero size if the pointer
+	 * is non-NULL, exit early if we do not need to allocate anything.
+	 */
+	if (out_count == 0)
+		return NULL;
+
 	out_count = def->out_bufs + (def->out_nums && 1);
 	out = malloc(sizeof(struct fastrpc_io_buffer) * out_count);
 	if (out == NULL)

commit 1dc590d638baabebcbb3d724903ff6966de020ee

fastrpc: hexagonrpcd: listener: compute out_count before zero check
The amount of output buffers needed should be computed before the check,
otherwise it checks an uninitialized value. Initialize it first so that
the zero check works as intended.

Fixes: 9ea9bed77b18 ("fastrpc: hexagonrpcd: listener: do not attempt to allocate zero outbufs")

fastrpc/hexagonrpcd/listener.c
@@ -73,6 +73,7 @@ static struct fastrpc_io_buffer *allocate_outbufs(const struct fastrpc_function_
 	off_t off;
 	uint32_t *sizes;
 
+	out_count = def->out_bufs + (def->out_nums && 1);
 	/*
 	 * POSIX allows malloc to return a non-NULL pointer to a zero-size area
 	 * in memory. Since the code below assumes non-zero size if the pointer
@@ -81,7 +82,6 @@ static struct fastrpc_io_buffer *allocate_outbufs(const struct fastrpc_function_
 	if (out_count == 0)
 		return NULL;
 
-	out_count = def->out_bufs + (def->out_nums && 1);
 	out = malloc(sizeof(struct fastrpc_io_buffer) * out_count);
 	if (out == NULL)
 		return NULL;